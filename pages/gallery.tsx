a60d3ec8bbbf96a3f23cbe411aec0d2fcc364fd2
Quoc <nguyenhungquoc1410@gmail.com>
2023-02-02 07:35:38 +0700

    [ADD] load images in get static props

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,26 +1,43 @@
-import { db } from '@/utils/firebase'
+import { db, storage } from '@/utils/firebase'
 import { DocumentData, doc, getDoc } from 'firebase/firestore'
+import { getDownloadURL, listAll, ref } from 'firebase/storage'
 import type { NextComponentType, NextPageContext } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 
 interface Props {
     data: DocumentData
+    urls: string[]
 }
 
 export const getStaticProps = async () => {
     const docRef = doc(db, 'content', 'gallery')
     const docSnap = await getDoc(docRef)
+    const galleryRef = ref(storage, 'Thumbnails')
+    const gallerySnap = await listAll(galleryRef)
+    let i = 0
+    let urls: string[] = []
+
+    while (i < gallerySnap.items.length) {
+        const url = await getDownloadURL(gallerySnap.items[i])
+        urls.push(url)
+        i++
+    }
+
     if (docSnap.exists()) {
         return {
-            props: { data: docSnap.data() },
+            props: { data: docSnap.data(), urls: urls },
         }
     } else {
         console.log('No such document!')
     }
 }
 
-const Gallery: NextComponentType<NextPageContext, {}, Props> = ({ data }) => {
+const Gallery: NextComponentType<NextPageContext, {}, Props> = ({
+    data,
+    urls,
+}) => {
+    console.log(urls)
     return (
         <div className="flex flex-wrap justify-center gap-4">
             {data.concepts.map((concept: string, index: number) => {
