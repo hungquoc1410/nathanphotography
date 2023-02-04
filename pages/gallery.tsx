0370d3b396ecebbc09d06da8b04dc842ae76d6d1
Quoc <nguyenhungquoc1410@gmail.com>
2023-02-04 14:34:28 +0700

    [ADD] gallery page

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,69 +1,65 @@
-import { db, storage } from '@/utils/firebase'
-import { DocumentData, doc, getDoc } from 'firebase/firestore'
+import { storage } from '@/utils/firebase'
 import { getDownloadURL, listAll, ref } from 'firebase/storage'
 import type { NextComponentType, NextPageContext } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 
 interface Props {
-    data: DocumentData
-    urls: string[]
+    data: { name: string; url: string }[]
+}
+
+const formatString = (string: string) => {
+    return string.split('-').join(' ')
 }
 
 export const getStaticProps = async () => {
-    const docRef = doc(db, 'content', 'gallery')
-    const docSnap = await getDoc(docRef)
-    const galleryRef = ref(storage, 'Thumbnails')
+    const galleryRef = ref(storage, 'thumbnails')
     const gallerySnap = await listAll(galleryRef)
     let i = 0
-    let urls: string[] = []
+    let data: { name: string; url: string }[] = []
 
     while (i < gallerySnap.items.length) {
         const url = await getDownloadURL(gallerySnap.items[i])
-        urls.push(url)
+        const name = gallerySnap.items[i].name.split('.')[0]
+        data.push({ name: name, url: url })
         i++
     }
 
-    if (docSnap.exists()) {
-        return {
-            props: { data: docSnap.data(), urls: urls },
-        }
-    } else {
-        console.log('No such document!')
+    return {
+        props: { data: data },
     }
 }
 
-const Gallery: NextComponentType<NextPageContext, {}, Props> = ({
-    data,
-    urls,
-}) => {
-    console.log(urls)
+const Gallery: NextComponentType<NextPageContext, {}, Props> = ({ data }) => {
     return (
         <div className="flex flex-wrap justify-center gap-4">
-            {data.concepts.map((concept: string, index: number) => {
-                return (
-                    <div
-                        className="w-5/12 rounded-lg bg-gray-200 p-1 lg:w-1/5"
-                        key={`concept_${index}`}
-                    >
-                        <figure className="group relative w-full cursor-pointer filter">
-                            <Link href="#">
+            {data.map(
+                (concept: { name: string; url: string }, index: number) => {
+                    return (
+                        <Link
+                            href={`/concepts/${concept.name}`}
+                            key={`concept_${index}`}
+                            className="w-5/12 rounded-lg bg-gray-200 p-1 lg:w-1/5"
+                        >
+                            <figure className="group relative w-full cursor-pointer filter">
                                 <Image
                                     priority
                                     className="block aspect-square h-full w-full rounded-lg object-cover object-center"
                                     width={500}
                                     height={500}
-                                    src="/mobile-background.webp"
+                                    src={concept.url}
                                     alt="image description"
                                 />
-                            </Link>
-                            <figcaption className="absolute left-0 bottom-0 flex w-full items-center justify-center rounded-b-lg bg-black/70 p-4 group-hover:h-full group-hover:rounded-lg group-hover:text-3xl">
-                                <p className="capitalize">{concept}</p>
-                            </figcaption>
-                        </figure>
-                    </div>
-                )
-            })}
+                                <figcaption className="absolute left-0 bottom-0 flex w-full items-center justify-center rounded-b-lg bg-black/70 p-4 group-hover:h-full group-hover:rounded-lg group-hover:text-3xl">
+                                    <p className="capitalize">
+                                        {formatString(concept.name)}
+                                    </p>
+                                </figcaption>
+                            </figure>
+                        </Link>
+                    )
+                }
+            )}
         </div>
     )
 }
